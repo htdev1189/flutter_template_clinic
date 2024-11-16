ff8616fa566aa6a08dc79de874e8a14bbcddf11c
hoangtuan <htdev1189@gmail.com>
2024-11-16 14:42:27 +0700

    edit file main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(title: 'Demo Template Clinic By Flutter'),
     );
   }
 }
